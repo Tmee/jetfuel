f021595fb59598a96955a8ced36d0bd4dce9a168
Timothy Mee <tjmee90@gmail.com>
2015-02-06 08:29:36 -0700

    spelling error, where the fuck were you on the one hound?

--- db/migrate/20150206050408_add_title_to_url.rb
+++ db/migrate/20150206050408_add_title_to_url.rb
@@ -1,5 +1,5 @@
 class AddTitleToUrl < ActiveRecord::Migration
   def change
-    add_column :urls, :title, :string, :default => 'No Title rFound'
+    add_column :urls, :title, :string, :default => 'No Title Found'
   end
 end
